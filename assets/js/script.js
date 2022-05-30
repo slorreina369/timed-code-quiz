6cc7d823c7352239a7aa336477d77148f02784e3
Lorreina Guyett <slorreina369@gmail.com>
2022-05-30 12:11:33 -0700

    function to display if answer is right or wrong setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@
 var cardElements = document.querySelectorAll(".card");
 var btnArray = document.querySelectorAll(".btn, .answer-btn");
 var answerBtns = document.querySelectorAll(".answer-btn");
+var answerHolder = document.querySelector(".answer-holder");
 
 var showNextCard = function(event){
     // find where we are
@@ -22,16 +23,25 @@ var showNextCard = function(event){
     nextCard.hidden = false;
     console.log(nextCard);
 };
-var correctAnswer = function(event){
-    // pull a question and an answer
-    var seekAnswer = event.target.dataset.correct
-    // compare questions and answers
 
+var correctAnswer = function(event){
+    // pull an answer
+    var seekAnswer = event.target.dataset.correct
+
+    answerHolder.hidden = false;
     // if/else what happens next
     if(!seekAnswer){
         console.log("wrong");
-    }
-}
+        answerHolder.innerText = "Wrong!";
+    } else {
+        console.log("correct");
+        answerHolder.innerText = "Correct!";
+    };
+    setTimeout(function(){
+        answerHolder.hidden = true
+    }, 2000);
+    
+};
 
 btnArray.forEach(function(elem){
     elem.addEventListener("click", showNextCard)
